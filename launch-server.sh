84d9110dbdbd38c2b23f160bf55a4e51c6091960
SamuelLup <lup@Genetica82.umhnet.es>
2020-04-29 11:14:34 +0200

    .

--- launch-server.sh
+++ launch-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # A simple script to launch the easymap server, takes one argument: a port number between 8100 and 8200
 
-port=$1
-
-nohup ./src/Python-2.7.18/.localpython/bin/python2 -m CGIHTTPServer $port &
+nohup ./src/Python-2.7.18/.localpython/bin/python2 -m CGIHTTPServer $1 &
