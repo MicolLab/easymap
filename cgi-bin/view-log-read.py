ba8e105bd6886efb37bfbf2bddef5ca132e2ef67
Valentina <jvalentinafr@gmail.com>
2024-12-16 09:27:36 +0100

    cgi.escape deprecated

--- cgi-bin/view-log-read.py
+++ cgi-bin/view-log-read.py
@@ -1,6 +1,6 @@
 #!src/Python-3.12.3/.localpython/bin/python3
 
-import cgi, cgitb
+import cgi, cgitb, html
 cgitb.enable()
  
 arguments = cgi.FieldStorage()
@@ -10,4 +10,4 @@ print("Content-type:text/html\r\n\r\n")
 
 with open('user_projects/' + projectName + '/2_logs/log.log') as logFile:
 	for line in logFile:
-		print(cgi.escape(line.strip()).encode('ascii', 'xmlcharrefreplace') + '<br>')
+		print(html.escape(line.strip()).encode('ascii', 'xmlcharrefreplace') + '<br>')
